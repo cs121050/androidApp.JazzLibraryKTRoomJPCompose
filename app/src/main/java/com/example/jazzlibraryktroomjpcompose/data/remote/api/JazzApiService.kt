cffe226c7f91147825c92fd58600b75f61baed2f
nickos sarr <nicko.sarr@gmail.com>
2026-01-21 19:51:29 +0200

    260121-add-dir-architecture-for-roomdb

---
+++ app/src/main/java/com/example/jazzlibraryktroomjpcompose/data/remote/api/JazzApiService.kt
@@ -0,0 +1,4 @@
+package com.example.jazzlibraryktroomjpcompose.data.remote.api
+
+class JazzApiService {
+}
---
+++ app/src/main/java/com/example/jazzlibraryktroomjpcompose/data/repository/ArtistRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.jazzlibraryktroomjpcompose.data.repository
+
+class ArtistRepositoryImpl {
+}
